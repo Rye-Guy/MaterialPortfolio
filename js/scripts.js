60234f8eb2e41c5259997ffa97a9baae2712b26d
Rye-Guy <ryanracioppo95@gmail.com>
2018-06-10 12:54:22 -0400

    holy shit this damn thing is working now

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 var didScroll;
-var lastScroll;
+var lastScroll = 0;
 var d = 5; 
-var navbarHeight = $("#navControl").outerHeight();
+var navbarHeight = $("header").outerHeight();
 
+//a function that lets the browser know when the user has scrolled that then triggers the setInterval. 
 $(window).scroll(function(event){
     didScroll = true; 
 });
 
+//check every 1/4 sec to see if the didScroll var state has changed. 
 setInterval(function(){
     if(didScroll){
         hasScrolled();
@@ -21,14 +23,18 @@ function hasScrolled(){
     //Makes sure the scroll is more than d
 
     if(Math.abs(lastScroll - st) <= d)
-    
+        return;
 
-    //what happens after you scroll past the navbar and were adding classes to control the bar. This is needed so we dont see emptpy space behind the navbar
+    //what happens after you scroll past the navbar and were adding classes to control the bar. This is needed so we dont see empty space behind the navbar
     if(st > lastScroll && st > navbarHeight){
         //Scroll Down
-        $("#navControl").removeClass('nav-down').addClass('nav-up');
+        $("header").removeClass('nav-down').addClass('nav-up');
     }else{
         //Scroll Up
-        $("#navControl").removeClass('nav-up').addClass('nav-down');
+        if(st + $(window).height() < $(document).height()){
+        $("header").removeClass('nav-up').addClass('nav-down');
+        }
     }
+    //value is set to current scroll position on the page. 
+    lastScroll = st;
 }
