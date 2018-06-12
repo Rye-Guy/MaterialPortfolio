f61296ab307997e35a87bdcd214d930149238358
Rye-Guy <ryanracioppo95@gmail.com>
2018-06-11 20:28:21 -0400

    added contact form modal and header content

--- js/scripts.js
+++ js/scripts.js
@@ -46,4 +46,31 @@ function hasScrolled(){
 
 //carousel initialization for materialize
     $('.carousel').carousel();
+
+//modal initialization for materialize
+    $('.modal').modal();
+
+
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+        
+          // Store hash
+          var hash = this.hash;
+          console.log(hash);
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 1200, function(){
+       
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        }
+      });
 });
